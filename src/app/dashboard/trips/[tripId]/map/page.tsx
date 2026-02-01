92fd7e7e8e63a1394c0680a4c13e10ac0acaaec2
mpoli <michelangelopoli21@gmail.com>
2026-02-01 18:54:56 +0100

    fixed map

--- src/app/dashboard/trips/[tripId]/map/page.tsx
+++ src/app/dashboard/trips/[tripId]/map/page.tsx
@@ -2,6 +2,7 @@
 
 import FirstLevelTripTemplate from "@/components/containers/first-level-trip-template";
 import PageTitle from "@/components/generics/page-title";
+import { useTrip } from "@/context/tripContext";
 import { appRoutes } from "@/utils/appRoutes";
 import dynamic from "next/dynamic";
 
@@ -11,12 +12,17 @@ const TripMap = dynamic(
 );
 
 export default function MapPage() {
+    const { trip } = useTrip();
     return (
         <FirstLevelTripTemplate
             breadcrumb={
                 [
                     {
                         label: 'I miei viaggi', href: appRoutes.home
+                    },
+                    {
+                        label: trip?.name || 'Dettagli Viaggio',
+                        href: appRoutes.tripDetails(trip?.id || '')
                     },
                     {
                         label: 'Mappa del Viaggio', href: ''

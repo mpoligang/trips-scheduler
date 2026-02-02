d7c1a9b9d3f3a46ca019bca2bd045c060649e545
mpoli <michelangelopoli21@gmail.com>
2026-02-02 17:51:09 +0100

    fixed map

--- src/components/maps/map.tsx
+++ src/components/maps/map.tsx
@@ -35,9 +35,17 @@ export default function MapPicker({ value, initialPosition = [45.4642, 9.19] }: 
 
     return (
         <MapContainer center={initialPosition} zoom={13} style={{ height: '400px', width: '100%' }} className="rounded-lg z-0">
+
+
             <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.jpg?key=tsbYUs7fzxQTOnUAVi0Z&language=en"
+                attribution='&copy; <a href="https://www.maptiler.com/copyright/">MapTiler</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+                tileSize={512}
+                zoomOffset={-1}
+                minZoom={1}
+                maxZoom={20}
+                detectRetina={true}
             />
 
             {/* Mostra il marcatore se c'è una posizione selezionata */}
